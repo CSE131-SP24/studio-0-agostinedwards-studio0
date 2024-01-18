9aa8d769ee52498439b12ac06f5a66f289a8c6de
edwardsr <edwardsr@1V000WL-98S1LR3.accounts.ad.wustl.edu>
2024-01-18 12:42:58 -0600

    Done

--- src/studio0/BurritoBudget.java
+++ src/studio0/BurritoBudget.java
@@ -9,7 +9,8 @@ public class BurritoBudget {
 		int burritosEatenWeek4 = 1;
 		
 		int totalBurritos = burritosEatenWeek1 + burritosEatenWeek2 + burritosEatenWeek3 + burritosEatenWeek4;
-		double averageWeeklyBurritos = totalBurritos / 4;
+		double x = 4;
+		double averageWeeklyBurritos = totalBurritos / x;
 		
 		double burritoPrice = 7.50;
 		double averageWeeklyPrice = averageWeeklyBurritos * burritoPrice;
